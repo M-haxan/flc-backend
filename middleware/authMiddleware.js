2a07b37400724c8d71a7dd45b909e258ec2d9fc3
Muhammad Hassan <hassanbalouch716@gmail.com>
2026-04-11 22:53:41 +0500

    Change storage type local to session for JWT

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken');
-
 // Yeh hamara middleware function hai
 const protect = (req, res, next) => {
     try {
@@ -10,27 +9,21 @@ const protect = (req, res, next) => {
             // 'Bearer ' ko hata kar sirf asli token nikal rahe hain
             token = req.headers.authorization.split(' ')[1];
         }
-
         // 2. Agar token bilkul hi nahi bheja gaya
         if (!token) {
             return res.status(401).json({ message: "Not authorized, no token provided" });
         }
-
         // 3. Token ko verify karein (apne secret password ke sath)
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
         // 4. Sabse zaroori step: Decoded data (jisme userId aur role chupa tha) 
         // usko hum 'req.user' mein daal dete hain taake aage route mein use ho sake.
         req.user = decoded;
-
         // 5. Guard ne check kar liya sab theek hai, ab aage jane do!
         next(); 
-
     } catch (error) {
         // Agar token expire ho gaya ho ya fake ho
         console.error("Token Verification Error:", error.message);
         res.status(401).json({ message: "Not authorized, invalid token" });
     }
 };
-
 module.exports = { protect };
